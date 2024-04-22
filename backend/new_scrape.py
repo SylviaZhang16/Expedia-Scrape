f868e3ec13fc410039b7e81b6e43f26d7051c575
zshihui16@gmail.com <sylviaboom16@gmail.com>
2024-04-22 08:52:58 -0400

    Edit style

--- backend/new_scrape.py
+++ backend/new_scrape.py
@@ -1,5 +1,9 @@
-import time
+
 from playwright.sync_api import sync_playwright
+import time
+from urllib.parse import quote_plus
+from datetime import datetime
+
 
 def fetch_hotels(destination, start_date, end_date):
     with sync_playwright() as pw:
@@ -10,11 +14,12 @@ def fetch_hotels(destination, start_date, end_date):
         url = f"https://www.expedia.com/Hotel-Search?destination={destination_encoded}&flexibility=0_DAY&d1={start_date}&startDate={start_date}&d2={end_date}&endDate={end_date}&adults=2&rooms=1"
         page.goto(url)
         time.sleep(2) 
+
         try:
             show_more = page.locator("text='Show More'")
             while show_more.is_visible():
                 show_more.click()
-                time.sleep(5) 
+                page.wait_for_load_state('networkidle')
         except Exception as e:
             print(f"Error clicking 'Show More': {e}")
 
@@ -24,10 +29,18 @@ def fetch_hotels(destination, start_date, end_date):
 
         for card in cards:
             title = card.locator('h3:not(.is-visually-hidden)').text_content()  
-            price_locator = card.locator('div.uitk-type-500')
-            price = price_locator.text_content() if price_locator.is_visible() else 'N/A'
+            price_locator = card.locator('div[data-test-id="price-summary-message-line"] div.uitk-type-200:has-text("total")')
+            price= price_locator.text_content() if price_locator.is_visible() else 'N/A'
+            image_locator = card.locator("figure.uitk-image img.uitk-image-media").first
+            # image_url = image_locator.get_attribute('src') if image_locator.is_visible() else 'default-image.jpg'
+            if image_locator.is_visible():
+                image_url = image_locator.get_attribute('src')
+            else:
+                image_locator.wait_for(state="visible")
+                image_url = image_locator.get_attribute('src') if image_locator.is_visible() else 'default-image.jpg'
 
-            hotel = {'name': title, 'price': price}
+            hotel = {'name': title, 'image_url': image_url, 'price': price}
+            # hotel = {'name': title,  'price': price}
             hotels.append(hotel)
             if len(hotels) >= 15:
                 break
